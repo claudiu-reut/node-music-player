e7ba1d051f1937d08c6d0862bf4399ac58e9eb8b
claudiu-reut <claudiu.reut@student.usv.ro>
2023-02-01 00:07:01 +0200

    working cover image

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
 
 import './App.css';
-
+import axios from 'axios'
 import { useState, useEffect } from 'react';
 function App() {
+  
   const [image,setImage]=useState();
   const [artist,setArtist]=useState('');
   const [name,setName]=useState('')
-  const [year,setyear]=useState('');
- 
-
+  const [year,setYear]=useState('');
+ const get_song=async ()=>{
+  try{
+    const response=await axios.get('http://localhost:4000/metadata');
+    const res=await axios.get('http://localhost:4000/image')
+    const metadata=response.data
+    setArtist(metadata.artist);
+    setName(metadata.title);
+    setYear(metadata.recordingTime);
+    if (res.data) setImage(`data:image/jpeg;base64,${res.data}`)
+    else setImage('https://cdn-icons-png.flaticon.com/512/27/27223.png')
+  }catch(err){
+    console.log(err);
+  }
+ }
+ useEffect(() => {
+ get_song();
+},[])
   return (
     
    
@@ -17,7 +33,7 @@ function App() {
       <h2>Playing Now</h2>
       <img
         className="musicCover"
-        src="https://picsum.photos/200/200"
+      src={image}
       />
       <div>
         <h3 className="title">{artist}</h3>
@@ -25,7 +41,7 @@ function App() {
         <p className="subTitle">{year}</p>
       </div>
       <div>
-      <img src="http://localhost:4000/image" alt='https://picsum.photos/200/200'/>
+      
     <audio controls>
         
         <source src="http://localhost:4000/audio" type="audio/mpeg"/>
--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const fs = require("fs")
+var cors = require('cors');
+
+
+
 const app =express();
 const NodeID3 = require('node-id3')
+app.use(cors({origin: '*'}));
 app.listen(4000,()=>{console.log("running on port 4000");})
-
+const image={
+    mime: "image/png",
+  type: {
+    id: 1
+  }, // See https://en.wikipedia.org/wiki/ID3#ID3v2_embedded_image_extension
+  description: "image description",
+  imageBuffer: fs.readFileSync('cover.jpg')
+}
+const tags = NodeID3.read('./sample.mp3');
+tags.image=image;
+NodeID3.write(tags,'./sample.mp3');
 app.get('/audio',(req,res)=>{
     const range = req.headers.range;
     const path = './sample.mp3'
@@ -26,9 +41,20 @@ app.get('/audio',(req,res)=>{
     stream.pipe(res);
 })
 app.get('/image',(req,res)=>{
-    
+    try{
     const tags = NodeID3.read('./sample.mp3')
 NodeID3.read('./sample.mp3', function(err, tags) {
-    console.log(tags);
+    res.status(200).json(tags.image.imageBuffer.toString('base64'));
 })
+    }catch(err){console.log(err);}
+})
+
+app.get('/metadata',(req,res)=>{
+    try{
+    const tags = NodeID3.read('./sample.mp3')
+NodeID3.read('./sample.mp3', function(err, tags) {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.status(200).json(tags)
+})
+    }catch(err){console.log(err);}
 })
